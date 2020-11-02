f6a049999738eb538387fecf7b810bd8af1990fb
GritskovaKseniya <grickovaKS@gmail.com>
2020-11-03 00:15:30 +0300

    .

--- App/taskmanager/main/models.py
+++ App/taskmanager/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import CharField
+
+
+class Grade (models.Model):
+    lesson: CharField = models.CharField(max_length=50)
