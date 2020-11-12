746f515a6020b17441395006b2bc47974d644824
RodionOsmekhin <rodionosmehin@gmail.com>
2020-11-12 19:09:49 +0300

    Добавил авторизацию. Тестовый пользователь логин: test пароль rad12345

--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
-
+# from django.urls import include, path
+from django.conf.urls import include, url
+from django.urls import path
 
 urlpatterns = [
     path('', include('app1.urls')),
     path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
 ]
