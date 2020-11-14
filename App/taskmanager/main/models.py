7642a0dcb8478baa3820cc6e5c551f4144e1e4d7
RodionOsmekhin <rodionosmehin@gmail.com>
2020-11-14 08:08:07 +0300

    Убрал ненужый пробный проект App

--- App/taskmanager/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db.models import CharField
-
-
-class Grade (models.Model):
-    lesson: CharField = models.CharField(max_length=50)
